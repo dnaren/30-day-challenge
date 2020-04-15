91589e41db54c3fb509dbd03578f56b307c7b563
Naren <narendiran@gmail.com>
2020-04-15 17:43:51 -0500

    day 15 base solution

--- Program.cs
+++ Program.cs
@@ -7,23 +7,12 @@ namespace ThirtyDayChallenge
   {
     static void Main(string[] args)
     {
-
-
-      var s = "xqgwkiqpif";
-      var shift = new int[][] {
-        new[] { 1, 4 }
-        , new[] { 0, 7 }
-        , new[] { 0, 8 }
-        , new[] { 0, 7 }
-      , new[] { 0, 6 }
-      , new[] { 1, 3 }
-      , new[] { 0, 1 }
-      , new[] { 1, 7 }
-      , new[] { 0, 5 }
-      , new[] { 0, 6 }};
-      var obj = new Day14PerformStringShifts();
-
-      Console.WriteLine(obj.StringShift(s, shift));
+      var input = new[] { 1, 2 };
+      var obj = new Day15ProductOfArrayExceptSelf();
+      foreach (var item in obj.ProductExceptSelf(input))
+      {
+        Console.WriteLine(item);
+      }
     }
   }
 }
